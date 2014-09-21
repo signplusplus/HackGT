d782b1623917c3186ae7b7d7927668f989970438
maye-vaca <madeleyne.vaca@gmail.com>
2014-09-20 23:35:47 -0400

    more flex sensor modification. ALL THE SENSORS NOW

--- sign++/flex_test/flex_test.ino
+++ sign++/flex_test/flex_test.ino
@@ -1,7 +1,8 @@
 int flexSensorPin = A0;
 int flexSensorPin2 = A1;
-int flexSensorPin3 = A3;
-int flexSensorPin4 = A5;
+int flexSensorPin3 = A3;                                          
+int flexSensorPin4 = A4;
+int flexSensorPin5 = A5;
 
 void setup() {
   // put your setup code here, to run once:
@@ -14,10 +15,12 @@ void loop() {
   int flexSensorReading2 = analogRead(flexSensorPin2);
   int flexSensorReading3 = analogRead(flexSensorPin3);
   int flexSensorReading4 = analogRead(flexSensorPin4);
+  int flexSensorReading5 = analogRead(flexSensorPin5);
   
   Serial.print("1st: ") ; Serial.println(flexSensorReading);
   Serial.print("2nd: ") ; Serial.println(flexSensorReading2);
   Serial.print("3rd: ") ; Serial.println(flexSensorReading3);
   Serial.print("4th: ") ; Serial.println(flexSensorReading4);
+  Serial.print("5th: ") ; Serial.println(flexSensorReading5);
   delay(1000);
-}                                                                                                                        
+}                                                                                                                                                                                                     
